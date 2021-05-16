7c7d60af27b13e45720dacce50722993f327a370
cmadusankahw <madushan_im16046@stu.kln.ac.lk>
2021-05-17 02:01:21 +0530

    descriptive analysis on datasets- notebooks

---
+++ data_insights.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+bb_usage_info = pd.read_csv("data/BB-Usage-Information.csv")
+
+bb_usage_info.shape()
+bb_usage_info.head()
